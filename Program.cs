3d3172ea57244444d0f0eb2b5c027d40a4cf20ed
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-10-30 11:58:35 +0000

    Initialise repo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Commander
+{
+    // this is the application's entry point
+    public class Program
+    {
+        // this is the main function that runs first & creates a host using IHostBuilder
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
