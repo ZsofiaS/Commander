701990f18e073d4da7dad7af39f578abcc0fab51
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-11-11 10:50:25 +0000

    Create 2 API endpoints: GET all commands, GET command by Id

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
@@ -6,9 +9,22 @@ namespace Commander.Controllers
     // [Route("api/[controller]")] 
     [Route("api/commands")]
     [ApiController]
-    public class CommandsController : ControllerBase
+    public class CommandsController : ControllerBase // inherits from Controller Base class (built in)
     {
-        // inherits from Controller Base class (built in)
-
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+        // this gives us a route to the following action result: GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
     }
 }
